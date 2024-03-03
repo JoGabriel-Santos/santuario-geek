ed990362380770880148ee746a2ff43bd6498386
Gabriel Santos <jgabriel.professional@gmail.com>
2024-03-03 18:19:27 -0300

    Implementing user login

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
 
     return (
-        <h5>Navbar</h5>
+        <header className={`header ${isNavOpen ? 'nav-open' : ''}`}>
+            <div className="logo-navigation">
+                <a href="#">
+                    <img className="logo" src={require("../util/img/logo_2.png")} alt=""/>
+                </a>
+
+                <nav className="main-navigation">
+                    <ul className="main-navigation-list">
+                        <li><a className="main-navigation-link" href="#">Home</a></li>
+                        <li><a className="main-navigation-link" href="#">Produtos</a></li>
+                        <li><a className="main-navigation-link" href="#">Contatos</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="navigation-icons">
+                <i className="navigation-icon fa-solid fa-cart-shopping"></i>
+                <i className="navigation-icon fa-solid fa-user"></i>
+            </div>
+
+            <button className="button-navigation-mobile" onClick={toggleNav}>
+                <ion-icon className="icon-navigation-mobile" name="menu-outline"></ion-icon>
+                <ion-icon className="icon-navigation-mobile" name="close-outline"></ion-icon>
+            </button>
+        </header>
     );
 };
 
