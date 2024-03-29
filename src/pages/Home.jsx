8ef7411155df88f6b5c732f5ad15d0cca2c03934
Gabriel Santos <jgabriel.professional@gmail.com>
2024-03-28 22:07:04 -0300

    Updating the home frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,34 @@ const Home = () => {
 
     return (
         <React.Fragment>
-
+            <section className="section-hero padding-b-100 next">
+                <div className="cr-slider swiper-container">
+                    <div className="swiper-wrapper">
+                        <div className="swiper-slide">
+                            <div className="cr-banner-image-one">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-lg-12">
+                                            <div className="cr-left-side-contain slider-animation">
+                                                <h5><span>Consequuntur</span> lorem</h5>
+                                                <h1>Santuário Geek</h1>
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                    Amet reiciendis beatae
+                                                </p>
+                                                <div className="cr-last-buttons">
+                                                    <a href="#" className="cr-button">
+                                                        Ver produtos
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     );
 };
